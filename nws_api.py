9aa0ceba36a72e2665f0a986183e02cc93bda14e
Steve <stagethird@gmail.com>
2024-02-21 19:25:28 -0600

    Incremental commit

--- nws_api.py
+++ nws_api.py
@@ -1,52 +1,45 @@
 import requests
 import json
+import sys
 
 class NWS:
     def __init__(self, lat: str, long: str):
         self.lat = lat
         self.long = long
         self.locationPage = None
-        self.targetURL = ""
+        self.dailyTargetURL = ""
+        self.hourlyTargetURL = ""
         self.targetCity = ""
         self.targetState = ""
+        self.dailyPage = None
+        self.dailyPeriodsList = []
+
+        self._location()
 
     def _location(self):
+        # add try block here
         self.locationPage = requests.get(f"https://api.weather.gov/points/{self.lat},{self.long}")
         _ = self.locationPage.json()
-        self.targetURL = _['properties']['forecast']
+        self.dailyTargetURL = _['properties']['forecast']
+        self.hourlyTargetURL = _['properties']['forecastHourly']
         self.targetCity =  _['properties']['relativeLocation']['properties']['city']
         self.targetState = _['properties']['relativeLocation']['properties']['state']
 
-if __name__ == "__main__":
-    me = NWS("45", "-93")
-    me._location()
-    print(me.targetURL)
-    print(me.targetCity)
-    print(me.targetState)
+    def get_daily_forecast(self):
+        try:
+            self.dailyPage = requests.get(self.dailyTargetURL)
+            dict1 = self.dailyPage.json()
+            self.dailyPeriodsList = dict1['properties']['periods']
+            return self.dailyPeriodsList
 
+        except requests.exceptions.ConnectionError:
+            print("ConnectionError: Site not reachable\n", dailyTargetURL)
+            sys.exit(1)
 
-
- #    def get_daily_forecast(lat, long):
- #        try:
- #            # locationPage = requests.get(f"https://api.weather.gov/points/{lat},{long}")
- #            url, city, state = location(locationPage)
- #            page = requests.get(url)
- #            dict1 = page.json()
- #            periodsList = dict1['properties']['periods']
- #            raise ValueError("Just kidding!")
- #            breakpoint()
- #            return (city, state, periodsList)
-
- #        except requests.exceptions.ConnectionError:
- #            print("ConnectionError: Site not reachable")
- #            sys.exit(1)
-
- #        except Exception as e:
- #            print("Exception:", e)
- #            print("Error retriving data. Urls queried:", locationPage.url, page.url)
- #            print(f"Title: {dict1['title']}, Status: {dict1['status']}")
- #            print(dict1['detail'])
- #            sys.exit(1)
+        except Exception as e:
+            print("Exception:", e)
+            print("in NWS.set_daily_forecast().")
+            sys.exit(1)
 
  #    def get_hourly_forecast(lat, long):
  #        try:
@@ -69,3 +62,14 @@ if __name__ == "__main__":
  #            print(f"Title: {dict1['title']}, Status: {dict1['status']}")
  #            print(dict1["detail"])
  #            sys.exit(1)
+
+if __name__ == "__main__":
+    me = NWS("45", "-93")
+    print(type(me.get_daily_forecast()))
+
+    # me._location()
+    # print(me.dailyTargetURL)
+    # print(me.targetCity)
+    # print(me.targetState)
+
+

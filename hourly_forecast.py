5fa06c02ff31982457f6814056ca037715edbf6d
Steve <stagethird@gmail.com>
2024-06-14 05:39:59 -0500

    Modified 'hourly_forecast.py'.
    The hourly forecast plot figure now conatins a second plot showing
    the percent chance for precipition. In addition, plot colors were
    updated to differentiate the two graphs. Shading fill under the plot
    line was added as well.

--- hourly_forecast.py
+++ hourly_forecast.py
@@ -69,18 +69,30 @@ if __name__ == '__main__':
     df["startTime"] = pd.to_datetime(df["startTime"])
     df["endTime"] = pd.to_datetime(df["endTime"])
 
-    figure = plt.figure(figsize=(10, 6))
-    ax = figure.add_subplot(111)
-    myFmt = mdates.DateFormatter("%m/%d")
-    ax.xaxis.set_major_formatter(myFmt)
-    ax.xaxis.grid(True, which="major")
-    ax.yaxis.grid(True, which="major")
-    ax.xaxis.set_minor_locator(mdates.HourLocator())
-    ax.set(
-        xlabel="Hourly Measurement",
-        ylabel="Degrees F",
-        title=f"Hourly Forecast: {city}, {state}",
-    )
-    ax.plot(df.startTime, df.temperature, ".-")
+    df2 = pd.json_normalize(df.probabilityOfPrecipitation)
+    df.probabilityOfPrecipitation = df2.value
 
+    figure = plt.figure(figsize=(10, 6))
+    ax1 = plt.subplot(2, 1, 1)
+    myFmt = mdates.DateFormatter("%m/%d")
+    ax1.xaxis.set_major_formatter(myFmt)
+    ax1.xaxis.grid(True, which="major")
+    ax1.yaxis.grid(True, which="major")
+    ax1.xaxis.set_minor_locator(mdates.HourLocator())
+    ax1.set_ylabel("Degrees F", color='maroon')
+    ax1.plot(df.startTime, df.temperature, ".-", color='maroon')
+    ax2 = plt.subplot(2, 1, 2, sharex=ax1)
+    ax2.xaxis.grid(True, which="major")
+    ax2.yaxis.grid(True, which="major")
+    ax2.set_ylabel("Prob. of Precip.", color='purple')
+    ax2.set(xlabel="Hourly Measurement")
+    ax2.plot(df.startTime, df.probabilityOfPrecipitation, ".-", color='purple')
+    for ax in ax1, ax2:
+        ax.grid(True)
+        ax.label_outer()
+
+    ax1.fill_between(df.startTime, df.temperature.min()-1, df.temperature, color='maroon', alpha=.1)
+    ax2.fill_between(df.startTime, df.probabilityOfPrecipitation.min(), df.probabilityOfPrecipitation,
+                 color='purple', alpha=.1)
+    figure.suptitle(f"Hourly Forecast: {city}, {state}")
     plt.show()
